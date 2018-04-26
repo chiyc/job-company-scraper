7f78dba67af5711a99bf8039bb2e50e7f2bb14bc
Chi Choi <chi.choi@socialtables.com>
2018-04-26 17:08:24 -0400

    initial commit for working Indeed scraper

---
+++ export.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const readline = require('readline');
+const fs = require('fs');=
+const stringify = require("csv-stringify");
+
+console.log("Beginning export...");
+
+const outputFile = "output.csv";
+
+const columns = {
+    job_title: "Job Title",
+    company_name: "Company Name",
+    company_location: "Company Location"
+};
+
+let contents = fs.readFileSync("output.json").toString().split("\n")
+.forEach(line => {
+    const obj = JSON.parse(line);
+});
+
+// iterate through each output json, remove .amenities array and replace with additional
+// snake-cased properties with a YES/NO desigination for whether or not it exists for that property
+const transformedData = [];
+contents = fs.readFileSync("output.json").toString().split("\n")
+.forEach(line => {
+    // for each amenity, add the snake_case property to obj with YES/NO value
+    // total_meeting_space sometimes has a newline (\n\) character in it - remove it
+    const obj = JSON.parse(line);
+    transformedData.push(obj);
+});
+
+stringify(transformedData, { header: true, columns: columns }, (err, output) => {
+    if (err) throw err;
+    fs.writeFile(outputFile, output, (err) => {
+        if (err) throw err;
+        console.log(`Export of ${outputFile} complete`);
+    });
+});
---
+++ index.js
@@ -0,0 +1,93 @@
+// inspired by:  http://toddhayton.com/2015/03/20/scraping-with-casperjs/
+// github:  https://github.com/thayton/casperjs-taleo-job-scraper/blob/master/scraper.js
+
+const job = "worker";
+const jobLocation = "Washington";
+const searchUrl = "https://www.indeed.com/jobs?q=" + job + "&l=" + jobLocation;
+var data = [];
+var currentPage = 1;
+var fs = require("fs");
+var utils = require("utils");
+var fileOutput = fs.open("output.json", 'w');
+
+var casper = require("casper").create({
+    pageSettings: {
+        loadImages: false,
+        loadPlugins: false,
+        verbose: false,
+        // userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/64.0.3282.186 Mobile/13B143 Safari/601.1.46",
+        userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+    }
+}); // DONE
+
+var getSelectedPage = function() {
+    var elem = document.querySelector("div.pagination > b");
+    return parseInt(elem.textContent);
+}
+
+var getCompanyData = function() {
+    var jobCards = document.querySelectorAll("div[class*='clickcard']");
+    var data = [];
+    console.log("Found " + jobCards.length + " entries on page");
+    for (var i=0; i<jobCards.length; i++) {
+        var row = jobCards[i];
+        var info = {};
+
+        var jobTitle = row.querySelector("a[data-tn-element='jobTitle']");
+        info["job_title"] = jobTitle.textContent;
+
+        var companyName = row.querySelector("span[class='company']");
+        info["company_name"] = companyName.textContent.replace(/\n    /g, "");
+
+        var companyLocation = row.querySelector("span[class='location']");
+        info["company_location"] = companyLocation.textContent;
+        data.push(info);
+    }
+    console.log(data);
+    return data;
+}
+
+var processPage = function() {
+    global.console.log("Started!");
+    // step 1:  click Quick View to expand contents of all properties
+    // casper.capture("screenshots/page" + currentPage);
+    global.console.log("Before evaluating getCompanyData()");
+    casper.capture("screenshots/test");
+    var results = this.evaluate(getCompanyData);
+    global.console.log("After evaluating getCompanyData()");
+    for (var c=0; c<results.length; c++) {
+        fileOutput.writeLine(JSON.stringify(results[c]));
+    }
+    utils.dump(results);
+
+    // step 2:  exit if we're finished
+    // if (!this.exists("li[class^='VenueList__listItem']")) {
+    //     return terminate.call(casper);
+    // }
+
+    // step 3:  click the next link to load more results
+    currentPage++
+    this.waitForSelector("a:last-child > span.pn > span.np", function() {
+        this.thenClick("a:last-child > span.pn > span.np").then(function() {
+            global.console.log("Clicking into next page...");
+            this.waitFor(function() {
+                return currentPage === this.evaluate(getSelectedPage);
+            }, processPage, terminate);
+        });
+    }, terminate);
+}
+
+var terminate = function() {
+    casper.capture("screenhots/test");
+    this.echo("Finishing scraping...").exit();
+    fileOutput.close();
+}
+
+global.console.log("Starting scraping... " + searchUrl);
+casper
+    .on("error", function(msg){ this.echo("error: " + msg, "ERROR") })
+    .on("page.error", function(msg, trace){ this.echo("Page Error: " + msg, "ERROR") })
+    //.on("remote.message", function(msg){ this.echo("Info: " + msg, "INFO") })
+    .start(searchUrl);
+casper.waitForSelector(".pagination", processPage, terminate);
+casper.run();
