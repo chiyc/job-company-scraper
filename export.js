aa8d5e55541d621835fb81126b91ca322c70c968
Chi Choi <chi.choi@socialtables.com>
2018-05-31 17:15:03 -0400

    dedupe and filter results during export

--- export.js
+++ export.js
@@ -17,12 +17,17 @@ const columns = {
 // iterate through each output json, remove .amenities array and replace with additional
 // snake-cased properties with a YES/NO desigination for whether or not it exists for that property
 const transformedData = [];
+const jobs = {};
 let contents = fs.readFileSync("output.json").toString().split("\n")
+.filter(line => line.toLowerCase().includes("merchand"))
 .forEach(line => {
     // for each amenity, add the snake_case property to obj with YES/NO value
     // total_meeting_space sometimes has a newline (\n\) character in it - remove it
-    const obj = line ? JSON.parse(line) : {};
-    transformedData.push(obj);
+    if (!jobs[line]) {
+        jobs[line] = true;
+        const obj = line ? JSON.parse(line) : {};
+        transformedData.push(obj);
+    }
 });
 
 stringify(transformedData, { header: true, columns: columns }, (err, output) => {
